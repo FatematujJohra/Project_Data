1177d6f7cfc0d9b209ac403ceb7bdc5fb8c1f705
Fatema Tuj Johra <johrafatema73@gmail.com>
2026-08-29 11:38:54 +0600

    Add files via upload

---
+++ etl/Basic.py
@@ -0,0 +1,36 @@
+print("Hello,World!")
+
+name="Johra"
+age=22
+print(name)
+print(age)
+
+#data type = string/int/float/bool
+name = "Fatema"
+age =22
+cgpa = 3.65
+passed = True
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(passed))
+
+#taking input
+name = input("Enter your name:")
+print("Hello", name)
+
+age = int(input("Enter your age:"))
+print("Age is:",age)
+
+#simple calculator
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Sum =",a+b)
+print("Difference =",a-b)
+print("Multiplication =",a*b)
+print("division =",a/b)
+#different operator are
+print("Floor/Int division =",a//b)
+print("Remainder = ",a%b)
+print("Power =",a**b)
+
